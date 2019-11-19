9644dd88d377c0017acebcd90de673d96b6df5e8
Max <knurt@hotmail.de>
2019-11-19 22:00:11 +0100

    fixed save feature, extended inheritance. Also edited vcx.xml.

--- Alpha/app/src/main/java/com/game/game/alpha/MainGame.java
+++ Alpha/app/src/main/java/com/game/game/alpha/MainGame.java
@@ -1,10 +1,14 @@
 package com.game.game.alpha;
 
+import android.content.Context;
+
+import com.game.alpha.MainActivity;
+
 import java.io.FileOutputStream;
 
 import static android.content.Context.MODE_PRIVATE;
 
-public class MainGame {
+public class MainGame extends MainActivity {
     private long counter;
     private String counterText;
 
@@ -22,11 +26,16 @@ public class MainGame {
         return counterText.valueOf(counter);
 
     }
-    public void saveInt(){
-        String saveTxt = String.valueOf(counter);
-        FileOutputStream intSave = openFileOutput("game_save_int_clicker.txt",MODE_PRIVATE);
-        intSave.write(saveInt().getBytes());
-        intSave.close();
+    public void saveInt(int toSave){
+        try {
+            String FILENAME = "gamesave.txt";
+            String saveTxt = String.valueOf(toSave);
+            FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+            fos.write(saveTxt.getBytes());
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
