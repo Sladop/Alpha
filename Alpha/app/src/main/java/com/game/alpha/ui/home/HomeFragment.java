e021aad7f849ba99946f29cab9985c94b0e6dca2
Philipp <philipp@famochs.de>
2019-11-20 15:08:02 +0100

    New @colors in color.xml also recolored
    Tried to design Buttons with rounded edges but failed.

--- Alpha/app/src/main/java/com/game/alpha/ui/home/HomeFragment.java
+++ Alpha/app/src/main/java/com/game/alpha/ui/home/HomeFragment.java
@@ -25,7 +25,6 @@ public class HomeFragment extends Fragment {
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
-        //final ImageView backImg = root.findViewById(R.id.backImg);
 
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
