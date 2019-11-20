69e268387f719afb483fc2c8a8d3840ae2a94cbb
Philipp <philipp@famochs.de>
2019-11-20 17:15:21 +0100

    +Button picture  + button but not working

--- Alpha/app/src/main/java/com/game/alpha/MainActivity.java
+++ Alpha/app/src/main/java/com/game/alpha/MainActivity.java
@@ -1,6 +1,7 @@
 package com.game.alpha;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -25,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        Button scpClicker = (Button)findViewById(R.id.scpClicker);
 
 
 
